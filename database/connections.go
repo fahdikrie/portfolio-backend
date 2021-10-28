8ec41a024c7e50247496d26e775176d37e5a0319
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-10-29 05:03:12 +0700

    feat(database): modify connections.go

--- database/connections.go
+++ database/connections.go
@@ -12,24 +12,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// DBinstance func
 func DBinstance() *mongo.Client {
-    err := godotenv.Load(".env")
-
-    if err != nil {
+    dotenv := godotenv.Load(".env")
+    if dotenv != nil {
         log.Fatal("Error loading .env file")
     }
 
     MongoDb := os.Getenv("MONGODB_URL")
-
     client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
     if err != nil {
         log.Fatal(err)
     }
 
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-
+    ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
     defer cancel()
+
     err = client.Connect(ctx)
     if err != nil {
         log.Fatal(err)
@@ -39,12 +36,9 @@ func DBinstance() *mongo.Client {
     return client
 }
 
-// Client Database instance
 var Client *mongo.Client = DBinstance()
 
-// OpenCollection is a  function makes a connection with a collection in the database
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-
     var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
 
     return collection
